c0416f263c367ecb8dc9a7c891c168086ab1ead4
Dino <25507425+Deaxz@users.noreply.github.com>
2021-03-12 16:05:13 +0100

    Lidt work

--- Value.c
+++ Value.c
@@ -8,19 +8,19 @@ typedef struct Node Node;
 typedef struct Linked_list;
 
 /* Node of double linked list */
-struct Node{
+struct Node {
   Value value;
   Node* prev;
   Node* next;
 };
 
 
-struct Linked_list{
+struct Linked_list {
   Node* head;
   void (*append)(Node** head, Value new_value);
 };
 
-void append(Node** head, Value new_value){
+void append(Node** head, Value new_value) {
   /* 1. Allocade node*/
   struct Node* new_node = (Node*)malloc(1, sizeof(Node));
   /* 2. Put data in node*/
@@ -33,9 +33,9 @@ void append(Node** head, Value new_value){
     *head = new_node;
     return;
   }
-  /* 5. Else traverse untill last node */
+  /* 5. Else traverse until last node */
   Node* temp = head;
-  while (temp->next != NULL){
+  while (temp->next != NULL) {
     temp = temp->next;
   }
   /* 6. Change the next of last node */
@@ -44,14 +44,25 @@ void append(Node** head, Value new_value){
     new_node->prev = temp;
 }
 
+/* Check whether p_value is in linked list */
+bool search(Node** head, Value* p_value) {
+  struct Node* current = head; // Make a node 'current' to search through linked list
+  while (current != NULL){
+    if (current->key == p_value)
+        return true;
+    current = current->next;
+  }
+  return false;
+}
 
-struct Value{
+
+struct Value {
   float data;
   float grad;
   Value (*_backward)(Value* self, Value* other);
   Value _prev[];
   char op[];
-}Value;
+} Value;
 
 
 int main() 
@@ -60,7 +71,7 @@ int main()
 }
 
 
-void backward(Value* self){
+void backward(Value* self) {
   Value topo[];
   Value visited[];
 
@@ -68,172 +79,42 @@ void backward(Value* self){
 
 }
 
-void build_topo(Value* v, Value* visited[]){
-  if(v_in_visited(*v, *visited) == false){
+void build_topo(Value* v, Value* visited[]) {
+  if (v_in_visited(*v, *visited) == false) {
     
   }
 }
 
 
-a = Value(2.0);
-
-b = Value(1.0);
-
-c = a + b; // Value(3.0)
-
-d = a * b + a**3 // Value(9.0) = Value(8.0) + Value(1.0)
-
-e = c * d // Value(27.0) = Value(3.0) * Value(9.0)
 
 
 
 
-e.backward()
+backward(*parent) {
 
-topo[a, b, c, d, e]
-visited = {e, c, a, b, d}
-
-e
-for {c, d}
-
-c
-for {a, b}
-
-a topo append
-
-c
-for {a, b}
-
-b topo append
-
-c topo append
-
-e
-for {c, d}
-
-d 
-for (a, b)
-
-a already visited
-
-d 
-for (a, b)
-
-b already visited
-
-d topo append
-
-e topo append
-
-
-reversed_topo[e, d, c, b, a]
-
-e._backward() = lambda
-
-d._backward() = out(a*b) + out(a**3)
-
-d._backward() = out(out(a*b) + out(a**3)) 
-
-
-
-c._backward() = { 
-  a.grad += c.grad 
-  b.grad += c.grad
-}
-
-
-
-
-
-d._backward() = out(out(a*b) + out(a**3)) 
-
-
-Value(a*b) = 2.0
-Value(a*b).grad = 0
-
-
-out(a*b) = {
-  a.grad += b.data * out.grad(???)
-}
-
-out(a**3) = {
-  a.grad += (3 * a.data**(3-1)) * out.grad(???)
-}
-
-a = value(1.0)
-b = value(2.0)
-
-a + b = value(a.data + b.data, (a,b), '+')
+  *Value topo; // Linkedlist
+  *Value visited; // Linkedlist
   
-c = value(a.data + b.data, (a,b), '+')
+  build_topo(*parent)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-a = Value(2.0);
-
-b = Value(1.0);
-
-d = a * b + a**3 // Value(10.0) = Value(8.0) + Value(1.0)
-
-
-d._backward();
-
-n + m
-
-n = out(a*b)
-m = out(a**3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-n.grad += out.grad
-m.grad += out.grad
-
-
-
-out(a*b) = {
-  a.grad += b.data * out.grad(???)
 }
 
-out(a**3) = {
-  a.grad += (3 * a.data**(3-1)) * out.grad(???)
+build_topo(*Value current) {
+
+  if v not in visited {
+    visited.add(current)
+    
+    for (i = 0; i < 2; i++) {
+
+      if (current.children[i] == 0)
+        return;
+
+      build_topo(current.children[i]);
+    }
+
+    topo.append(v);
+  }    
 }
 
 
@@ -267,26 +148,27 @@ out(a**3) = {
 
 
 
-f(g(x))
 
-f'(g(x)) * g'(x)
+d.backward()
 
-(2 * x)^2
-
-
-g(x) = 2 * x
-
-f(x) = x^2
+d._backward()
 
 
-g'(x) = 2
-f'(x) = 2x
 
-x = 4
+a = 2.0
+b = 1.0
 
+d = a * b + pow(a, 3)
 
-(2 * 2 * x) * (2) 
-f' (g(x)) * g'(x)
+a._backward = lambda
+b._backward = lambda
+
+(a*b)._backward = multbackward
+(pow(a,3))._backward = powbackward
+d._backward = plusbackward
+
+d.backward
+
 
 
 
