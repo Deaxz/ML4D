d53cae657eb34e664db8d072c078401149736d8e
Dino <25507425+Deaxz@users.noreply.github.com>
2021-05-17 14:38:31 +0200

    Nodes for 2.0 statements

--- ML4D/Compiler/AST-Builder/Nodes/StmtNodes.cs
+++ ML4D/Compiler/AST-Builder/Nodes/StmtNodes.cs
@@ -61,4 +61,67 @@ namespace ML4D.Compiler.Nodes
             return new List<Node>();
         }
     }
+    
+    // Nyt shit fra 2.0
+    public class IfElseNode : Node // TODO, make dis
+    {
+        public List<IfNode> IfNodes = new List<IfNode>();
+        public LinesNode? ElseBody = new LinesNode();
+        
+        public override List<Node> GetChildren()
+        {
+            if (ElseBody is null)
+            {
+                List<Node> children = new List<Node>();
+                return children.Concat(IfNodes).ToList();  // TODO tjek den returnere korrekt
+            }
+            return IfNodes.Concat(ElseBody.lines).ToList(); // TODO tjek den returnere korrekt
+        }
+    }
+    
+    public class IfNode : Node 
+    {
+        public ExpressionNode Predicate { get; set; }
+        public LinesNode Body { get; set; }
+        
+        public override List<Node> GetChildren()
+        {
+            List<Node> children = new List<Node>() {Predicate};
+            return children.Concat(Body.lines).ToList();
+        }
+    }
+
+    public class ForNode : Node // TODO, make dis
+    {
+        public AssignNode Init { get; set; }
+        public ExpressionNode Predicate { get; set; }
+        public AssignNode Final { get; set; }
+        public LinesNode Body { get; set; }
+
+        public override List<Node> GetChildren()
+        {
+            List<Node> children = new List<Node>()
+            {
+                Init, Predicate, Final
+            };
+            return children.Concat(Body.lines).ToList();
+        }
+    }
+
+    public class GradientsNode : Node // TODO, make dis
+    {
+        public string tensorID { get; set; }
+        public List<Node> GradVariables = new List<Node>(); // FunctionArgumentNode
+        public List<string> GradTensors = new List<string>();
+
+        public GradientsNode(string tensorId) // Arguments bliver lavet ligesom med funktioner
+        {
+            tensorID = tensorId;
+        }
+
+        public override List<Node> GetChildren()
+        {
+            return GradVariables;
+        }
+    }
 }
