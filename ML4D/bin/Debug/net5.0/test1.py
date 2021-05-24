5b7055f781dfce97e7b42233c47834cc9de80090
Janus Jonatan Hannesarson <janushannesarson@gmail.com>
2021-05-24 17:40:12 +0200

    fixed test1 examples

---
+++ ML4D/bin/Debug/net5.0/test1.py
@@ -0,0 +1,24 @@
+import torch
+
+a = torch.tensor([[-1.0, 412., -55.2],
+                  [77.1, 981., -48.]], requires_grad=True)
+
+b = torch.tensor([[-144., 412.],
+                  [-1., 5.],
+                  [55., 13.]], requires_grad=True)
+
+c = torch.matmul(a, b)
+
+d = torch.tensor([[1.], [-1.]], requires_grad=True)
+
+e = torch.matmul(c, d)
+
+f = torch.tensor([11., -19], requires_grad=True)
+
+result = torch.matmul(f, e)**0.5
+
+result.backward()
+
+print(a.grad)
+
+print(result)
